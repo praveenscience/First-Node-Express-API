d6c8a3ce516fa94bc5c95eddda1d9388a8ada558
Praveen Kumar <praveenscience@gmail.com>
2020-11-29 12:45:10 +0000

    Created a first GET endpoint.

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.json("Welcome to API Server!");
+});
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}.`);
 });
