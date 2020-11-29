b136ee6aa61f6e724f4a48509b5c8901cb112ae5
Praveen Kumar <praveenscience@gmail.com>
2020-11-29 13:27:50 +0000

    Implemented creating a new record using POST.

--- api/todo.js
+++ api/todo.js
@@ -6,5 +6,13 @@ let ToDos = [];
 app.get("/", (req, res) => {
   res.json(ToDos);
 });
+app.post("/", express.json(), (req, res) => {
+  if (typeof req.body.text !== "undefined") {
+    ToDos.push(req.body.text);
+    res.json(ToDos.length - 1);
+  } else {
+    res.status(400).json("You have to enter text.");
+  }
+});
 
 module.exports = app;
